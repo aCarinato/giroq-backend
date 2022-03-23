242041eb569f552afaf3f2bee80a32fc4a03a548
aCarinato <alessandro.carinato@gmail.com>
2022-03-23 19:25:02 +0100

    added controllers

---
+++ controllers/events.js
@@ -0,0 +1,37 @@
+const Event = require('../models/Event');
+
+exports.getEvents = async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.status(200).json(events);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getEventsDateRange = async (req, res) => {
+  try {
+    const { firstdate, lastdate } = req.params;
+    // console.log(`Dall API, firstdate: ${firstdate}`);
+    // console.log(`Dall API, lastdate: ${lastdate}`);
+    const formattedFirstDate = new Date(firstdate);
+    const formattedLastDate = new Date(lastdate);
+
+    const dayBeforeFirstDate = formattedFirstDate.setDate(
+      formattedFirstDate.getDate() - 1
+    );
+
+    const dayAfterLastDate = formattedLastDate.setDate(
+      formattedLastDate.getDate() + 1
+    );
+
+    // console.log(`Dall API, dayBeforeFirstDate: ${dayBeforeFirstDate}`);
+    // console.log(`Dall API, dayAfterLastDate: ${dayAfterLastDate}`);
+    const events = await Event.find({
+      date: { $gt: dayBeforeFirstDate, $lt: dayAfterLastDate },
+    });
+    res.status(200).json(events);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
