dd7705779cf7da06f5b41882451556ee1c4d395d
aCarinato <alessandro.carinato@gmail.com>
2022-05-10 11:40:09 +0200

    remove past events

--- routes/eventsRoutes.js
+++ routes/eventsRoutes.js
@@ -4,19 +4,22 @@ const router = express.Router();
 
 import {
   getEvents,
-  getEventsDateRange,
+  // getEventsDateRange,
   postEvents,
+  getPastEvents,
 } from '../controllers/events.js';
 
 //get all events
 router.get('/', getEvents);
 
+router.get('/past-events', getPastEvents);
+
 router.post('/', postEvents);
 
-router.get(
-  // '/:firstdate/:lastdate/:blLat/:trLat/:blLong/:trLong',
-  '/:firstDate/:lastDate/:trLat/:trLong/:blLat/:blLong',
-  getEventsDateRange
-);
+// router.get(
+//   // '/:firstdate/:lastdate/:blLat/:trLat/:blLong/:trLong',
+//   '/:firstDate/:lastDate/:trLat/:trLong/:blLat/:blLong',
+//   getEventsDateRange
+// );
 
 export default router;
